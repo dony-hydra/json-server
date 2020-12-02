2f5d26afe40f98e9994177c4a3bbbf9723f33d2c
trantrungisk5 <trantrungisk5@gmail.com>
2020-12-02 15:55:35 +0700

    [Add] Read file and write file

--- index.ts
+++ index.ts
@@ -1,36 +1,26 @@
 const jsonServer = require("json-server");
+const fs = require("fs");
 // import * as faker from "faker";
-const faker = require("faker");
 
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 5000;
 
+let mockup: any;
+
+// Read file DB
+fs.readFile("mockup.json", (err, data) => {
+  if (err) throw err;
+  mockup = JSON.parse(data);
+  console.log(mockup);
+  fs.writeFileSync("db.json", JSON.stringify(mockup));
+});
+
 // Set default middleware
 server.use(middleware);
-
-function generateEmployees() {
-  var employees = [];
-
-  for (var id = 0; id < 50; id++) {
-    var firstName = faker.name.firstName();
-    var lastName = faker.name.lastName();
-    var email = faker.internet.email();
-
-    employees.push({
-      id: id,
-      first_name: firstName,
-      last_name: lastName,
-      email: email,
-    });
-  }
-
-  return { data: employees };
-}
-
 // server.get("/api/employees", (req, res) => {
-//   res.status(200).json(generateEmployees());
+//   res.status(200).json();
 // });
 
 server.use(router);
